19e84b7a27727f5a78208a15700fd772ec36f6ab
unknown <yadavbharat71@gmail.com>
2026-01-04 14:51:55 +0530

    ui color and some changes

---
+++ lib/utill/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xff344FA1);
+const Color lightBlue = Color(0xffA1C0F8);
+const Color cardColor = Color(0xff031956);
+const Color accentColor = Color(0xffEB05FF);
